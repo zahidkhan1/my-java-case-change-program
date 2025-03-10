54c7d30c96b06bfd7580462e5b9eb4133a33279d
Zahid khan <zahidkhan43743@gmail.com>
2025-03-10 16:25:22 +0500

    java case change

---
+++ app.js
@@ -0,0 +1,46 @@
+// let num2 =  [1,3,5,7,9,11,13,15,17,19,20,21,23,25]
+// let theEven = false
+// let forcount=0
+// for (let i = 0;i<num2.length;i++){
+// forcount++
+// if (num2[i] % 2 == 0){
+//     theEven = true
+//     break
+// }
+
+
+// }
+// console.log("chal" + forcount);
+// if (theEven){
+//     console.log("the num is even")
+    
+    
+// }
+// else{
+//     console.log("no even nuum ");
+    
+// }
+// let names=["zahid","zohaib","zakir","ikram","zarwali","zaman"]
+// let name = false
+// forcount = 0
+// for(let i=0;i<names.length;i++){
+// forcount++
+// if(names[i] == "waseem"){
+//     console.log("true")
+//     break
+// }
+// }
+// console.log("chal "+ forcount);
+// if (names) {
+//     console.log("that is  waseem");
+    
+// }
+// else
+// {
+//     console.log("no waseem");
+    
+// }
+//........................for case change..............
+let myname="zahid KhAN"
+let myNewname=myname.toLowerCase()
+console.log(myNewname);
